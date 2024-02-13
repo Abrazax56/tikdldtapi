14d3cd898d36ea6c250fc458e85c4fbc7dd67080
abrazax56 <ahmadbenirusli@gmail.com>
2024-02-13 14:23:53 +0700

    part-008

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,24 @@
 import express from "express";
 import { TiktokDL } from "@tobyg74/tiktok-api-dl";
-import cors from "cors"
+import cors from "cors";
+import fs from 'fs'
 
 const app = express();
 const port = 3000;
+const htmls = fs.readFileSync('./main.html');
 
 app.use(cors());
 app.set('json spaces', 2);
 
 app.get('/', (req, res) => {
-  res.json({
+  res.send(htmls.toString());
+  /*res.json({
       "name": "tikdldtapi",
       "author": "ahmadbenirusli",
       "version": "1.0.0",
       "usage": "https://tikdldtapi.vercel.app/download?url=<YOUR_TIKTOK_URL>",
       "caution": "this api is not legal just experimental, don't use for production"
-    });
+    });*/
 });
 
 app.get('/download', async(req, res) => {
@@ -38,6 +41,7 @@ app.get('/download', async(req, res) => {
 app.use('/', (req, res) => {
   res.status(404);
   res.json({
+      "request_status": 404,
       "name": "tikdldtapi",
       "author": "ahmadbenirusli",
       "version": "1.0.0",
