35846b1445cb74300975024b40fdc2dc571faf50
abrazax56 <ahmadbenirusli@gmail.com>
2024-02-16 13:54:51 +0700

    part-008

--- api/index.ts
+++ api/index.ts
@@ -51,7 +51,7 @@ const resultHTMLvideo = ( status: string, type: string, music: string, video: st
 const resultHTMLimage = ( status: string, type: string, music: string, images: any ) => {
   let imageRess: string = ``;
   images.forEach((imgUrl: string) => {
-    imageRess += `<span class="value">"<a href="${imgUrl}"target="_blank">${imgUrl}</a>",</span> `
+    imageRess += `<span class="value">"<a href="${imgUrl}"target="_blank" class="value">${imgUrl}</a>",</span> `
   })
   return `
 <!DOCTYPE HTML><html lang="en"><head><meta charset="UTF-8"/><meta name="viewport" content="width=device-width, initial-scale=1.0"/><meta name="description" content="Tiktokapidownloader"/><meta name="theme-color" content="black"/><title>tikdldtapi</title><style>*, body, html{font-family: Monospace;margin: 0;padding: 0;}*::selection{background-color:rgba(0, 200, 255, 1);color:white;}body{width: 100%;min-height: 100vh;background-color: rgba(0, 10, 30, 1);box-sizing: border-box;}.box{width: 100%;box-sizing: border-box;color: white;}.props{color: salmon;font-weight: 1000;}.kurung{color: gold;}.value{color: rgba(0, 255, 200, 1);}.pre{line-height: 20px;padding: 20px;}</style></head><body><div class="box">
